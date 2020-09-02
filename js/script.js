8ab359c6419d7b46e138ab267f607d1eb35e71b4
Bjornson-dev <69739502+Bjornson-dev@users.noreply.github.com>
2020-09-02 18:27:34 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,5 +14,22 @@ $(document).ready(function () {
   $("#form").submit(() => {
     alert("Спасибо! Ваша заявка принята! Скоро мы с Вами свяжемся.")
   });
-
+  $("#formModal").submit(() => {
+    alert("Спасибо! Ваша заявка принята! Скоро мы с Вами свяжемся.")
+  });
+  $(".modal-btn").click(() => {
+    $(".modal").addClass("show");
+  })
+  $(".closeModal").click(() => {
+    $(".modal").removeClass("show");
+  })
+  jQuery(function ($) {
+    $(document).mouseup(function (e) {
+      var div = $(".modalWrapper");
+      if (!div.is(e.target)
+        && div.has(e.target).length === 0) {
+        $(".modal").removeClass("show");
+      }
+    });
+  });
 });
